855109f22cf83c5883ee65c8cc59f9939ed34d6d
fundiar <eroffe@gmail.com>
2023-05-16 11:10:00 -0300

    formatting comments

--- game.js
+++ game.js
@@ -1,34 +1,47 @@
 
-const buttonColors = ["red", "blue", "green", "yellow"]; //array with fixed colors
-var rndnbr; //Store random number to chose a random color from above array
-var randomChosenColor; //Translate random color to actual color
+//Available colors for patterns
+const buttonColors = ["red", "blue", "green", "yellow"];
 
-var gamePattern = []; //Store game pattern
-var userClickedPattern = []; //Store user clicked pattern
-var userClickedColor; //User clicked translated color
-var gameLevel = 0; // track game level
+ //Store random number to chose a random color from above array
+var rndnbr;
+
+//Translate random color to actual color
+var randomChosenColor; 
+
+//Store game pattern
+var gamePattern = []; 
+
+ //Store user clicked pattern
+var userClickedPattern = [];
+
+//User clicked translated color
+var userClickedColor;
+
+// track game level
+var gameLevel = 0; 
 
 
+//Start Game
 $(this).on("keypress", function () {
+    //new color in the sequence
     rndnbr = nextSequence();
     randomChosenColor = buttonColors[rndnbr];
     gamePattern.push(randomChosenColor);
 
+    //flash & sound for new color in sequence
     flashButton("#" + randomChosenColor);
     playColorAudio(randomChosenColor);
    
-
+    //capture user input
     $('.btn').on("click", function () {
     
         playColorAudio($(this).attr("id"));
         flashButton("#" + $(this).attr("id"));
     
     })
-
 });
 
-
-
+//Select next sequence in the pattern
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
  
@@ -36,11 +49,13 @@ function nextSequence() {
     return randomNumber;
 }
 
+//Play the right audio for each color
 function playColorAudio(color) {
     var audio = new Audio("sounds/"+color + ".mp3");
     audio.play();
 }
 
+//Flash the right button for each color. 
 function flashButton(color) {
     $(color).addClass("pressed");
     setTimeout(function () {
